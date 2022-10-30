11fb8b59305a8dafc9339ae5cda5f4b6e8eaa131
IT21084522 <89662507+IT21084522@users.noreply.github.com>
2022-10-31 01:33:45 +0530

    Add files via upload

---
+++ roomConfirmation.java
@@ -0,0 +1,14 @@
+package com.example.roombooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class roomConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_booking);
+    }
+}
